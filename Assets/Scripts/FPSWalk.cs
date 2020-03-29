315764ee8ecb1c587b3071bef7cde1be40a454be
Rodrigo Santos Alvares <ralvares@EGSBR.local>
2020-03-29 15:34:42 -0300

    Project Start

---
+++ Assets/Scripts/FPSWalk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSWalk : MonoBehaviour
+{
+
+    public CharacterController character;
+    public Vector3 positionToGo;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionToGo = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diftowalk = positionToGo - transform.position;
+
+        character.SimpleMove(diftowalk.normalized);
+
+        if (character.velocity.magnitude > 0.4 && !audioSource.isPlaying){
+            audioSource.volume = Random.Range(0.75f, 1);
+            audioSource.pitch =  Random.Range(0.75f, 1);
+            audioSource.Play();
+        }
+       
+    }
+}
